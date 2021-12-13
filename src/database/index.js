e83563c807b66841b60039bac26fcf1f78e5df6f
강찬석 <ssimjunlke@naver.com>
2021-12-13 21:39:22 +0900

    서버 배포 완료

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,12 @@
 const db = require('knex')({
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
-      user : 'board_user',
+      // host : '127.0.0.1',
+      // host : 'noolim-project.cdhxtabtekrk.ap-northeast-2.rds.amazonaws.com',
+      host : 'springboot2-webservice.covzzjccqpoz.ap-northeast-2.rds.amazonaws.com',
+      user : 'sjoongh',
       port : 3306,
-      password : 'board',
+      password : 'sjh87354~',
       database : 'board_db',
       timezone: 'KST'
     },
